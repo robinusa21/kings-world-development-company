8a53aadebef0bf517d0e20b29251e591cc85bfbf
Muzahidul Parvez <81441097+robinusa21@users.noreply.github.com>
2022-02-13 17:36:17 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    /*Type_js*/
+    var typed = new Typed('.typed', {
+        strings: ["DEGITAL MARKETING.", "CONTENT MARKETING.", "WEB DEVELOPMENT.", "APP DEVELOPMENT.", "ONLINE DIRECTORY LISTINGS.", "SEARCH ENGINE OPTIMIZATION.", "APP SEARCH OPTIMIZATION."],
+        typeSpeed: 60,
+        backSpeed: 50,
+        loop: true
+        
+      });
+});
+
+new WOW().init();
+
+document.querySelector("#info-circle-wrapper-6204d32517b06 > div.info-c-full-br");
+
